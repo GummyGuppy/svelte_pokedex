568f2a574bc48d819341b4015d919819c5d34eeb
GummyGuppy <alphastack.dev@gmail.com>
2022-10-07 20:40:38 -0500

    updated pokdex

---
+++ src/components/Button.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let fn, title
+</script>
+
+<button class="bg-gradient-to-r active:bg-gradient-to-l from-slate-200 to-blue-300 
+    py-2 px-6 rounded-lg border-blue-300 border-2 active:outline active:outline-2 
+    active:outline-blue-400 active:outline-spacing-2 active:text-lg"
+    on:click={fn}>{title}</button>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,56 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+    import "../app.css";
+    import Button from '../components/Button.svelte'
+
+	
+	$: currentPokemon = 1// 0
+	$: pokemon = []
+	
+	let value = ''
+		
+	//fetch pokemon from PokeAPI
+	function getPokemon(){
+		fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+			.then(response => response.json())
+			.then(allpokemon =>  {
+			allpokemon.results.map((poke) => pokemon = [...pokemon, poke.name])})
+	
+		
+			
+	}	
+	//call function
+	getPokemon()
+
+	//back functionality
+	function back(){
+		if(currentPokemon > 1){
+			currentPokemon--
+		}
+	}
+
+	//next functionality
+	function next(){
+		if(currentPokemon < pokemon.length - 1)
+			currentPokemon++
+	}
+	
+</script>
+
+<div class="flex flex-col w-screen gap-6 items-center">
+
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{currentPokemon}.png"/>
+    <p>
+        current pokemon is {pokemon[currentPokemon - 1]}
+    </p>
+
+    <div class="flex gap-4">
+        <Button fn={back} title="Back"/>
+        <Button fn={next} title="Next"/>
+    </div>
+
+
+
+</div>
+
+
